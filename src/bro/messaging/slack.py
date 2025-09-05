648766d78b72fe089c5bb2b301cdd90dc6d7c2b1
Huong Pham <email.me@0w.ee>
2025-09-05 21:59:08 +0300

    add logging, process text message with attachments

--- src/bro/messaging/slack.py
+++ src/bro/messaging/slack.py
@@ -1,9 +1,8 @@
+import logging
 import os
 from time import sleep
-from typing import Type
 
 from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 from slack_sdk.socket_mode import SocketModeClient
 from slack_sdk.socket_mode.request import SocketModeRequest
 from slack_sdk.socket_mode.response import SocketModeResponse
@@ -13,6 +12,11 @@ from bro.messaging import Messaging, Channel, ReceivedMessage, Message
 
 bot_token = os.environ["SLACK_BOT_TOKEN"]
 app_token = os.environ["SLACK_APP_TOKEN"]
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
 
 class SlackMessaging(Messaging):
     def __init__(self) -> None:
@@ -31,9 +35,24 @@ class SlackMessaging(Messaging):
             response = SocketModeResponse(envelope_id=req.envelope_id)
             client.send_socket_mode_response(response)
             if req.payload["event"]["type"] == "message":
-                self.unread_msgs.append(ReceivedMessage(via=req.payload["event"]["channel"],
-                                                        text=req.payload["event"]["text"],
-                                                        attachments=req.payload["event"]["attachments"]))
+                attachments = []
+                text = ''
+                channel_id = req.payload["event"]["channel"]
+                if req.payload["event"].get("text"):
+                    text = req.payload["event"]["text"]
+                    logging.info("Received a text message.")
+                if req.payload["event"].get("subtype") == "file_share":
+                    logging.info("Received one file or more.")
+                    files = req.payload["event"]["files"]
+                    for file in files:
+                        file_info = self.web_client.files_info(file=file["id"])
+                        file_download_url = file_info["file"]["url_private_download"]
+                        attachments.append(file_download_url)
+                logging.info("Received a total of %d files." % len(attachments))
+                self.unread_msgs.append(ReceivedMessage(via=Channel(name=channel_id),
+                                                        text=text,
+                                                        attachments=attachments))
+                # TODO fetch attachments, not just download links
 
     def list_channels(self) -> list[Channel] | SlackResponse:
         response: SlackResponse = self.web_client.conversations_list(
@@ -47,6 +66,7 @@ class SlackMessaging(Messaging):
     def poll(self) -> list[ReceivedMessage]:
         last_unread_msgs = self.unread_msgs
         self.unread_msgs = []
+        # add mutex everywhere
         return last_unread_msgs
 
     def send(self, message: Message, via: Channel) -> None:
