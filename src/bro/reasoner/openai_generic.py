d623b0a79db3063647c568bf3dd05fc84070f14d
Pavel Kirienko <pavel.kirienko@gmail.com>
2025-09-12 00:37:43 +0300

    oops

--- src/bro/reasoner/openai_generic.py
+++ src/bro/reasoner/openai_generic.py
@@ -456,9 +456,9 @@ class OpenAiGenericReasoner(Reasoner):
         for x in addendum:
             if x.get("type") == "reasoning" and "status" in x:
                 del x["status"]
-            if out.get("type") == "web_search_call":
+            if x.get("type") == "web_search_call":
                 try:
-                    del out["action"]["sources"]
+                    del x["action"]["sources"]
                 except (TypeError, LookupError, ValueError):
                     pass
 
